755a21d4d642ea9e64bc1f9cb04783290f811aa8
maiiaKey <59762050+maiiaKey@users.noreply.github.com>
2020-02-01 12:23:15 +0100

    Updates

---
+++ precache-manifest.65e02679e07e9a48476695aec108a5f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f83c1eef43c1be2f1e978ab436140e9a",
+    "url": "/EasySchool/index.html"
+  },
+  {
+    "revision": "7150fd4ae2bf34c027b3",
+    "url": "/EasySchool/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6a18c8f13afab2779680",
+    "url": "/EasySchool/static/js/2.eb8d6734.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/EasySchool/static/js/2.eb8d6734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7150fd4ae2bf34c027b3",
+    "url": "/EasySchool/static/js/main.6fe85043.chunk.js"
+  },
+  {
+    "revision": "41e9712763cb9709fc9b",
+    "url": "/EasySchool/static/js/runtime-main.f04c2b17.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/EasySchool/static/media/logo.5d5d9eef.svg"
+  }
+]);
